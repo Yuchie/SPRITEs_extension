cbf952baeee6cea9c21a69ac759783abd7b437e7
YUQIAN SUN <ys47@uw.edu>
2018-05-22 23:53:12 -0700

    try to send dom from content_scripts to background by converting to string

--- js/background.js
+++ js/background.js
@@ -1,15 +1,25 @@
 "use strict";
 
-chrome.runtime.onMessage.addListener(function (message) {
-	console.log("I read");
-	console.log(message);
-	//document.getElementById('url').innerHTML = message.innerHTML;
-});
-
 window.onload = function() {
-	chrome.runtime.onMessage.addListener(function (message) {
-		console.log("I read");
-		console.log(message);
-		//document.getElementById('url').innerHTML = message.innerHTML;
-	});
+	var parser = new DOMParser();
+	chrome.runtime.onMessage.addListener(
+		function(request, sender, sendResponse) {
+		    if( request.message === "open_new_page" ) {
+		      	console.log("I read");
+		      	console.log(request.div);
+		      	let doc = parser.parseFromString(request.div, "text/xml");
+				console.log(doc.firstChild);
+				//document.getElementById('url').innerHTML = message.innerHTML;
+		    } else if (request.message === 'test') {
+		    	console.log("I read " + request.value);
+		    }
+		 });
 };
+
+// chrome.runtime.onMessage.addListener(function (msg, sender) {
+//   // First, validate the message's structure
+//   if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
+//     // Enable the page-action for the requesting tab
+//     chrome.pageAction.show(sender.tab.id);
+//   }
+// });
--- js/content_scripts/sample.js
+++ js/content_scripts/sample.js
@@ -1,12 +1,25 @@
 "use strict";
 
 window.onload = function() {
-	let button = document.getElementById("hplogo");
+	var serializer = new XMLSerializer();
 	// button.addEventListener("click", function() {
  //    	alert("clicked");
  //    	// chrome.runtime.sendMessage(button);
  //  	}, false);
  	let divlist = document.getElementsByTagName("DIV");
-  	console.log(divlist);
-  	chrome.runtime.sendMessage(divlist);
+  	console.log(divlist[0]);
+  	let sendDiv = serializer.serializeToString(divlist[0]);
+  	chrome.runtime.sendMessage({"message": "open_new_page", "div":sendDiv});
+  	console.log(sendDiv);
+  	chrome.runtime.sendMessage({"message": "test", "value":'test'});
+  // function(request, sender, sendResponse) {
+  //   if( request.message === "clicked_browser_action" ) {
+  //     var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+  //     console.log(firstHref);
+
+  //     // This line is new!
+  //     chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+  //   }
+  // }
 };
