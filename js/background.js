a2668df4f2d6a211ab3c535f2465805c2e5f064c
YUQIAN SUN <ys47@uw.edu>
2018-06-03 15:08:34 -0700

    add keyboard input listener

--- js/background.js
+++ js/background.js
@@ -7,31 +7,43 @@
 "use strict";
 
 window.onload = function() {
-	var parser = new DOMParser();
-
-	chrome.runtime.onMessage.addListener(
-
-		// ---------------------------------------
-		// Receive when the website is opened
-		// ---------------------------------------
-		function(request, sender, sendResponse) {
-		    if( request.from === 'content' && request.message === "open_new_page" ) {
-		      	console.log("I read");
-		      	let doc = parser.parseFromString(request.document, "text/xml");
-				console.log(doc);
-				console.log(doc.body.children.length)
-		    } else if (request.message === 'test') {
-		    	console.log("I read " + request.value);
-		    }
-		 }
-
-	);
+	chrome.runtime.onMessage.addListener(readMessage);
+	
 };
 
-// chrome.runtime.onMessage.addListener(function (msg, sender) {
-//   // First, validate the message's structure
-//   if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
-//     // Enable the page-action for the requesting tab
-//     chrome.pageAction.show(sender.tab.id);
-//   }
-// });
+
+// ---------------------------------------
+// Read Message from content_js
+// ---------------------------------------
+function readMessage(request, sender, sendResponse) {
+	if(request.from === 'content') {
+		switch(request.message) {
+			// read the whole web document
+			case 'open_new_page':
+				let doc = readDom(request.value);
+				break;
+			case 'key_input':
+				readKeyInput(request.value);
+				break;
+			default:
+				console.log('unexpected message');
+				break;
+		}
+	}
+}
+
+// ---------------------------------------
+// Read the document
+// ---------------------------------------
+function readDom(doc) {
+	let parser = new DOMParser();
+	let dom = parser.parseFromString(doc, "text/xml");
+	return dom;
+}
+
+// ---------------------------------------
+// Read keyboard input
+// ---------------------------------------
+function readKeyInput(key) {
+	console.log(key);
+}
--- js/content_scripts/loadDocument.js
+++ js/content_scripts/loadDocument.js
@@ -11,5 +11,18 @@ window.onload = function() {
 	var serializer = new XMLSerializer();
  	let wholeDocument = document;
   	let sendDocument = serializer.serializeToString(wholeDocument);
-  	chrome.runtime.sendMessage({"message": "open_new_page", "from": "content", "document":sendDocument});
+  	chrome.runtime.sendMessage({"message": "open_new_page", "from": "content", "value":sendDocument});
+
+  	window.addEventListener('keydown', keySend);
 };
+
+
+// -----------------------------------------------
+// send key input to the background
+// -----------------------------------------------
+function keySend(event) {
+	console.log("key pressed");
+	let key = event.code;
+	console.log(event);
+	chrome.runtime.sendMessage({"message": "key_input", "from": "content", "value": key});
+}
